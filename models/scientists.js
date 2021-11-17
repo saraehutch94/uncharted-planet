77a74e16b67312f7fc1d7b67cbb4f2cdcac38e27
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-11-17 16:40:16 -0500

    add show.ejs, show route, delete route

---
+++ models/scientists.js
@@ -0,0 +1,3 @@
+const scientists = [];
+
+module.exports = scientists;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // Require dependencies
 
+const { application } = require("express");
 const express = require("express");
 const methodOverride = require("method-override");
+const scientists = require("./models/scientists");
 
 // Initialize the application
 
@@ -30,12 +32,39 @@ app.use(methodOverride("_method"));
 
 // index route
 app.get("/planets", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {scientists});
 });
 
 // new route
 app.get("/planets/new", (req, res) => {
     res.render("new.ejs");
+});
+
+// delete route
+app.delete("/planets/:id", (req, res) => {
+    scientists.splice(req.params.id, 1);
+    res.redirect("/planets");
+});
+
+
+// update route
+
+
+// create route
+app.post("/planets", (req, res) => {
+    scientists.push(req.body);
+    res.redirect("/planets");
+});
+
+// edit route
+
+
+// show route
+
+app.get("/planets/:id", (req, res) => {
+    const foundScientist = scientists[req.params.id];
+    console.log(foundScientist);
+    res.render("show.ejs", {foundScientist});
 });
 
 // Tell app to listen for client requests
